44a4270eddf2cb789f751535288fac6f92e274f5
BillStrait <bill.strait@gmail.com>
2021-01-01 22:05:02 -0600

    Restored Node Code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+class TitleBanner extends React.Component{
+    render(){
+        return(
+            <h1 id="titleText">Bill Strait</h1>
+        );
+    }
+}
+
+class LinksList extends React.Component{
+    render(){
+        return(
+            <ul id="contactList">
+                <li className="contactListItem"><div className="contactText">Hire me: </div><a className="contactLink" href="https://www.upwork.com/fl/billstrait" target="_new">UpWork</a></li>
+                <li className="contactListItem"><div className="contactText">Look at some code: </div><a className="contactLink" href="https://www.github.com/billstrait" target="_new">GitHub</a></li>
+                <li className="contactListItem"><div className="contactText">See my resume: </div><a className="contactLink" href="https://www.linkedin.com/in/billstrait/" target="_new">LinkedIn</a></li>
+                <li className="contactListItem"><div className="contactText">Chat with me securely: </div><a className="contactLink" href="https://keybase.io/billdr" target="_new">Keybase</a></li>
+                <li className="contactListItem"><div className="contactText">Send me an email: </div><a className="contactLink" href="mailto:bill@billdlabs.com">Email</a></li>
+                <li className="contactListItem"><div className="contactText">Hang out: </div><a className="contactLink" href="https://www.twitch.tv/billd" target="_new">Twitch</a></li>
+            </ul>
+        );
+    }
+}
+
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <TitleBanner/>
+      <LinksList/>
     </div>
   );
 }
---
+++ src/index.test.js
@@ -0,0 +1,5 @@
+import { render, screen } from '@testing-library/react';
+
+test('just pass', ()=>{
+    expect(1).toBe(1);
+})
